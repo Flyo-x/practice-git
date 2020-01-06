1b1888393579c343900c335deb6e8f4597efa094
kevin <flyo_x@126.com>
2020-01-06 21:56:31 +0800

    测试添加文件

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, { 'content-type': 'text/html' })
     console.log('test')
     console.log('test')
-    console.log('test')
 })
 
 server.listen(3000, () => {
